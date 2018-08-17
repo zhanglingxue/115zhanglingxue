01d91c589b8416fc17b0b5c8536467c017c53789
zhanglingxue <1050080567@qq.com>
2018-08-17 14:01:19 +0800

    答辩

--- src/App.js
+++ src/App.js
@@ -1,30 +1,27 @@
-import React, { Component } from 'react'
-import { Provider } from 'react-redux'
-import thunk from 'redux-thunk'
-import rootReducer from './reducers'
-import { createStore, applyMiddleware, compose } from 'redux'
-import { createLogger } from 'redux-logger'
-import serverApi from './middleware/serverapi'
-import { browserHistory,Router } from 'react-router'
-import routes from './routes'
-import TopBarView from './container/leftBar'
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { createLogger } from 'redux-logger';
+import { browserHistory, Router } from 'react-router';
+import rootReducer from './reducers';
+import serverApi from './middleware/serverapi';
+import routes from './routes';
 
 const logger = createLogger();
 const store = createStore(
-    rootReducer,
-    compose(
-        applyMiddleware(thunk,serverApi,logger)
-    )
-)
+  rootReducer,
+  compose(applyMiddleware(thunk, serverApi, logger))
+);
 
 class App extends Component {
-    render() {
-        return (
-            <Provider store={store}>
-                <Router routes={routes} history={browserHistory}/>
-            </Provider>
-        )
-    }
+  render() {
+    return (
+      <Provider store={store}>
+        <Router routes={routes} history={browserHistory} />
+      </Provider>
+    );
+  }
 }
 
 export default App;
